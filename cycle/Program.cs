0527e4fc1a7f81a3eb89d4605087699c27325661
sergey <sergey.fomin.90@inbox.ru>
2022-08-15 23:28:33 +0400

    add_cycle_new

--- cycle/Program.cs
+++ cycle/Program.cs
@@ -1,10 +1,10 @@
-﻿for (int i = 2; i <= 10; i++)
-{
-    for (int j = 2; j <= 10; j++)
-    {
-        Console.WriteLine($"{i} x {j} = {i * j}");
-    }
-}
+﻿// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i * j}");
+//     }
+// }
 //=====Работа с текстом
 // Дан текст. В тексте нужно все пробелы заменить чёрточками,
 // маленькие буквы “к” заменить большими “К”,
@@ -27,5 +27,5 @@ string Replase(string text, char oldValue, char newValue)
     }
     return result;
 }
-string newText = Replase(text, " ", "|");
+string newText = Replase(text, ' ' , '|');
 Console.WriteLine(newText);
