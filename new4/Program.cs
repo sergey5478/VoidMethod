c4624ecb5b11bc1a1ea65c2d6dc1416c9406e3ea
sergey <sergey.fomin.90@inbox.ru>
2022-08-15 23:27:54 +0400

    add_new4

---
+++ new4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+LenNumber();
+void LenNumber()
+{
+   int[] ArrayM = new int[8];;
+   Random rnd = new Random();
+   for(int i=0; i<8; i++)
+   {
+    ArrayM[i] = rnd.Next(0,2);
+    Console.Write($"{ArrayM[i]} ");
+   }
+}
