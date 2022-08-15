3d7378dfbb7e2cbfbf34c7a5d68eb5562dab4a28
sergey <sergey.fomin.90@inbox.ru>
2022-08-15 23:27:27 +0400

    add_new2

---
+++ new2/Program.cs
@@ -0,0 +1,20 @@
+﻿// 26
+//Напишите программу, которая принимает 
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int getSumToA(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum++;
+        number = number / 10;
+    }
+    return sum;
+}
+Console.WriteLine($"Количество чисел {getSumToA(num)}");
