bc7ae015cfe3162e7f84a310d46e45f6fe81c012
sergey <sergey.fomin.90@inbox.ru>
2022-08-18 23:51:51 +0400

    add_tut2

---
+++ tut2/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задача 32: Напишите программу замена элементов
+массива: положительные элементы замените на
+соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2]
+*/
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+        Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+int[] array = GetArray(12, -9, 9);
+Console.WriteLine();
+
+int[] array1 = new int[array.Length];
+int j = 0;
+
+foreach (int i in array)
+{
+    if (i > 0)
+    {
+        array1[j] = i * -1;
+        j++;
+    }
+    else
+    {
+        array1[j] = i * -1;
+        j++;
+    }
+    Console.Write(j);
+}
