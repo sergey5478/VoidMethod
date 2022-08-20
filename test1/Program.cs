300af63bd8749a015ff9d04c7b26bf24fa1b8e77
sergey <sergey.fomin.90@inbox.ru>
2022-08-20 23:12:49 +0400

    add_test1

--- test1/Program.cs
+++ test1/Program.cs
@@ -1,9 +1,13 @@
 ﻿void RandomArray(int[] array)//Метод подсчета 
 {
-    for (int i = 0; i < a.Length; i++)//цикл прогона через весь массив/сравнение с 
+    for (int i = 0; i < array.Length; i++)//цикл прогона через весь массив/сравнение с 
     //длиной массива 
     {
         array[i]=new Random().Next(1, 100); //блок рандома который принимает 
         Console.Write($"{array[i]} ");//Вывод на экран
     }
 }
+// int[] arr =  new int [6];  // Можно задать переменную заранее
+// RandomArray(arr);
+
+RandomArray(new int [6]); //или прям в скобках
