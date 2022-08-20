0dcfb9f6abecb7e7071e4b93141ece5cd1f9a344
sergey <sergey.fomin.90@inbox.ru>
2022-08-20 23:13:04 +0400

    add_test2

--- test2/Program.cs
+++ test2/Program.cs
@@ -6,16 +6,15 @@
 int[] GetArray(int size, int minValue, int maxValue)
 {
     int[] res = new int[size];
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < res.Length; i++)
     {
-        res[i] = new Random().Next(minValue, maxValue + 1);
+        res[i] = new Random().Next(minValue, maxValue+1);
         Console.Write($"{res[i]} ");
     }
     return res;
 }
-int[] array = GetArray(12, -9, 9);
+int[] array = GetArray(5, -1, 2);
 Console.WriteLine();
-
 for(int i = 0; i < array.Length; i++)
 {
     array[i] = array [i] * -1;
