4d51962204304943a9ab30114ce25b93f5c8dffc
sergey <sergey.fomin.90@inbox.ru>
2022-08-20 23:10:40 +0400

    add_tut6-2

---
+++ tut6-2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 36: Задайте одномерный массив, 
+заполненный случайными числами. Найдите сумму элементов, 
+стоящих на нечётных позициях(по индексу считаем).
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < res.Length; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+        Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+int[] array = GetArray(4, -99, 99);
+Console.WriteLine();
+int Sum =0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 != 0)
+    {
+        Sum += array[i];
+    }
+}
+Console.WriteLine(Sum);
