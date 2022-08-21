5683bab8aa8b4e48a1156cebd1c52d2e49f1e569
sergey <sergey.fomin.90@inbox.ru>
2022-08-21 14:49:06 +0400

    add_test000

---
+++ test000/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int GetSumNums(int number)
+{
+    int sum = 0;
+    int i = 1;
+    while(i<=number)
+    {
+        sum +=i;
+        Console.Write($"{sum} ")
+        i++;
+    }
+    return sum;
+}
+Console.WriteLine($"Сумма индексов чисел от 1 до А = {GetSumNums(num)}");//хрень
