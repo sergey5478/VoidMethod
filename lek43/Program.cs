50a7799a69e2ec757f387048c2d7c74276fd9fa9
sergey <sergey.fomin.90@inbox.ru>
2022-08-21 14:48:45 +0400

    add_lek43

---
+++ lek43/Program.cs
@@ -0,0 +1,30 @@
+﻿//Рекурсия  =  ищем факториал
+//Когда факториал больше 17, то нехватает места
+//Меняем int yf doubl
+
+/*double Factorial(int n)
+{
+    //1! = 1
+    //0! = 1
+    if(n==1) return 1;
+    else return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+*/
+//Фибоначи
+//f(1) = 1
+//f(2) = 2
+//f(n) = f(n-1) + f(n-2)
+
+double Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 10; i++)
+{
+    Console.WriteLine($"f{i} = {Fibonacci(i)}");
+}
