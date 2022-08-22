33a78e918f662c749ef2d2b646b6b81eb32f16a5
sergey <sergey.fomin.90@inbox.ru>
2022-08-22 23:56:55 +0400

    add_sem63

---
+++ sem63/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 42: Напишите программу, которая будет 
+преобразовывать десятичное число в двоичное.
+45 -> 101101
+3  -> 11
+2  -> 10
+*/
+Console.WriteLine("Введите число ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int num = userNumber;
+int remains = 0;
+
+while(num !=0)
+{
+    num = userNumber / 2;
+    remains = num % 2;
+    Console.Write(remains);
+    
+}
