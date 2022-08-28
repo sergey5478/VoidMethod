db7c9411c9d602876cc6e2abddd0e11800318586
sergey <sergey.fomin.90@inbox.ru>
2022-08-28 22:29:37 +0400

    changes

---
+++ task47/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+Console.WriteLine("Введите размеры массива M и N: ");
+int lines = Convert.ToInt32(Console.ReadLine());
+int columns = Convert.ToInt32(Console.ReadLine());
+double[,] GetArray(int min, int max)
+{
+    double[,] result = new double[lines, columns];
+    for (int i = 0; i < lines; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(min, max)* new Random().NextDouble();
+            result[i, j] = Math.Round(result[i, j], 2);
+        }
+    }
+    return result;
+}
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+double[,] array = GetArray(-10, 10);
+PrintArray(array);
