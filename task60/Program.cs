6fe6440b182a9ed7f48bb8cc4a6517558016cd02
sergey <sergey.fomin.90@inbox.ru>
2022-09-01 20:17:20 +0400

    addtask60

---
+++ task60/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Задача 60: Сформируйте трёхмерный массив из неповторяющихся
+двузначных чисел. Напишите программу, которая будет построчно выводить
+массив, добавляя индексы каждого элемента.
+*/
+int[,,] array3D = new int[2, 2, 2];
+CreateArray(array3D);
+WriteArray(array3D);
+void WriteArray(int[,,] array3D)
+{
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            Console.Write($"X({i}) Y({j}) ");
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {
+                Console.Write($"Z({k})={array3D[i, j, k]}; ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+int[,,] CreateArray(int[,,] array)
+{
+    int sum = new Random().Next(1, 10);
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            for(int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i,j,k] = sum;
+                sum = sum + array[i,j,k];
+            }
+        }
+    }
+    return array;
+}
