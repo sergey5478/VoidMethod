a109e1665cf06c9719693db5ab11047861774f26
sergey <sergey.fomin.90@inbox.ru>
2022-09-01 20:15:53 +0400

    addtask53

---
+++ task53/Program.cs
@@ -0,0 +1,50 @@
+﻿/*Задача 53: Задайте двумерный массив. Напишите программу,
+которая поменяет местами первую и последнюю строку
+массива.
+*/
+Console.WriteLine("Введите количество строк");
+int raws = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ведите количество столбцов");
+int colums = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[raws, colums];
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 20);
+        }
+    }
+}
+
+int[,] ChangeArray(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        int temp = array[0, j];
+        array[0, j] =array[array.GetLength(0) - 1, j];
+        array[array.GetLength(0)-1 ,j] = temp;
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+ChangeArray(array);
+PrintArray(array);
