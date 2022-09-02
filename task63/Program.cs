dadce5d9af6eaf4b060e62923e7bec8e05fdcc1e
sergey <sergey.fomin.90@inbox.ru>
2022-09-02 23:07:56 +0400

    addtask63

---
+++ task63/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 63: Задайте значение N. Напишите программу, которая выведет
+все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"
+*/
+Console.Clear();
+Console.WriteLine(Number(4, 10));
+string Number(int a, int b)
+{
+    if (a <= b)
+    {
+        return $"{a} " + Number(a + 1, b);
+    }
+    else
+    {
+        return String.Empty;
+    }
+}
+
