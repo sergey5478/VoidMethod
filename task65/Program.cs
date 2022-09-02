3e2ca32fe2ae12d65c30787c97a7cc26c1959619
sergey <sergey.fomin.90@inbox.ru>
2022-09-02 23:08:16 +0400

    addtask65

---
+++ task65/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 65: Задайте значения M и N. Напишите программу, которая
+выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+M = 4; N = 8 -> "4, 6, 7, 8
+*/
+Console.Clear();
+Console.WriteLine("Введите a и b");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Number(a, b));
+string Number(int a, int b)
+{
+    if (a <= b)
+    {
+        return $"{a} " + Number(a + 1, b);
+    }
+    else
+    {
+        return String.Empty;
+    }
+}
+
